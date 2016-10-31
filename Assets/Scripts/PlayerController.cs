22c945315e40b2415dd455ce7d5dfe100f7a3973
James Woo <jamesjaywoo@gmail.com>
2016-10-31 12:09:24 -0700

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	[SerializeField]
+	private float _speed = 5f;
+	[SerializeField]
+	private float _mouseSensitivity = 3f;
+
+	private PlayerMotor _motor;
+	private Animator _animator;
+
+	// Use this for initialization
+	void Start() 
+	{
+		_motor = GetComponent<PlayerMotor>();
+		_animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		var xMov = Input.GetAxisRaw("Horizontal");
+		var zMov = Input.GetAxisRaw("Vertical");
+
+		// Movement
+		var movHorizontal = transform.right * xMov;
+		var movVertical = transform.forward * zMov;
+		var velocity = (movHorizontal + movVertical).normalized * _speed;
+		_motor.Move (velocity);
+		_animator.SetFloat("VSpeed", zMov);
+		_animator.SetFloat("HSpeed", xMov);
+
+		// Rotation
+		var yRot = Input.GetAxisRaw("Mouse X");
+		var xRot = Input.GetAxisRaw("Mouse Y");
+		var rotation = new Vector3(0.0f, yRot, 0.0f) * _mouseSensitivity;
+		var cameraRotation = new Vector3(xRot, 0.0f, 0.0f) * _mouseSensitivity;
+		_motor.Rotate(rotation);
+		_motor.RotateCamera(cameraRotation);
+
+        // Attacking
+        var attack = Input.GetButtonDown("Fire1");
+        if(attack) 
+        {
+            _animator.SetTrigger("Attacking");
+            Invoke("StopAttack", 0.01f);
+        }
+	}
+
+    void StopAttack()
+    {
+        _animator.SetTrigger("StopAttacking");
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+
+	[SerializeField]
+	private Camera _camera;
+
+	private Vector3 _velocity = Vector3.zero;
+	private Vector3 _rotate = Vector3.zero;
+	private Vector3 _cameraRotate = Vector3.zero;
+	private Rigidbody _rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() 
+	{
+		PerformMovement ();
+		PerformRotation ();
+	}
+
+	void PerformMovement()
+	{
+		if (_velocity != Vector3.zero) 
+		{
+			_rb.MovePosition(_rb.position + _velocity * Time.fixedDeltaTime);
+		}
+	}
+
+	void PerformRotation()
+	{
+		_rb.MoveRotation(_rb.rotation * Quaternion.Euler(_rotate));
+		if(_camera != null)
+		{
+			_camera.transform.Rotate(-_cameraRotate);
+		}
+	}
+
+	public void Move(Vector3 velocity)
+	{
+		_velocity = velocity;
+	}
+
+	public void Rotate(Vector3 rotate)
+	{
+		_rotate = rotate;
+	}
+
+	public void RotateCamera(Vector3 rotate)
+	{
+		_cameraRotate = rotate;
+	}
+}
